9539615ec6dd100fe37c717a931d940f02c3dc82
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-14 10:20:13 -0600

    init commit

--- inventario.php
+++ inventario.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="images/icons/Grammer_Logo.ico" type="image/x-icon">
+    <link rel="icon" href="" type="image/x-icon">
     <meta name="description" content="">
     <meta name="author" content="">
 
