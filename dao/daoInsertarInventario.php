f7fdb30f2a97a5301589dd58a2e359cde9f2cb77
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-15 12:15:56 -0600

    insert Area table

--- dao/daoInsertarInventario.php
+++ dao/daoInsertarInventario.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Respuesta final si todos fueron exitosos
         if ($todosExitosos) {
-            $respuesta = array("status" => 'success', "message" => "Todos los registros en la Tabla Parte fueron actualizados correctamente.");
+            $respuesta = array("status" => 'success', "message" => "Todos los registros en la Tabla InventarioSAP fueron actualizados correctamente.");
         } else {
             $respuesta = array("status" => 'error', "message" => "Se encontraron errores al insertar los registros.", "detalles" => $errores);
         }
