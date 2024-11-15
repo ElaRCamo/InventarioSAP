f27f51cb1356e5a6693ec25584b327a9dea364c4
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-15 08:01:34 -0600

    insert Ubicaciones table

--- inventario.php
+++ inventario.php
@@ -28,7 +28,7 @@
         <div class="container">
             <div class="row">
                 <div class="container mt-5">
-                    <h2 class="text-center">Tabla Storage</h2>
+                    <h2 class="text-center">Tabla Ubicaciones</h2>
                     <button class="btn btn-secondary text-right btnExcel" id="btnExcelUbicaciones"> Cargar Archivo</button>
                     <input type="file" id="fileInputUbicaciones" accept=".xlsx, .xls" style="display: none;" />
                     <table class="table table-striped table-bordered mt-3" id="tablaUbicaciones">
