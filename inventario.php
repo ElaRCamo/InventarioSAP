2548140c2a19ad338deb71471251246ce3254f13
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-19 11:43:36 -0600

    insert txt Bitacora

--- inventario.php
+++ inventario.php
@@ -224,6 +224,7 @@
 
 <!-- JAVASCRIPT FILES -->
 <script src="js/excel.js"></script>
+<script src="js/archivoTexto.js"></script>
 
 <!-- BOOSTRAP -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
