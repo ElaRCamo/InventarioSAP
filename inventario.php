e06fc2006ac296ebf871bbcd04f7418256bf40ef
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-22 07:55:03 -0600

    insert txt Storage_Unit

--- inventario.php
+++ inventario.php
@@ -37,8 +37,7 @@
                     <h2 class="text-center">Tabla Bitacora</h2>
                     <button class="btn btn-success text-right btnExcel" id="btnExcelBitacora"> Cargar Excel Bitacora</button>
                     <input type="file" id="fileInputBitacora" accept=".xlsx, .xls" style="display: none;" />
-                    <i class="far fa-question-circle position-absolute" id="tooltipBitacora"></i>
-                    <!--<button class="btn btn-secondary text-right btnExcel" id="tooltipBitacora">Ejemplo excel</button>-->
+                    <button class="btn btn-secondary text-right btnExcel" id="tooltipBitacora"><i class="far fa-question-circle position-absolute"></i>Ejemplo excel</button>
 
                     <button class="btn btn-primary text-right btnExcel" id="btnTxtBitacora"> Actualizar txt </button>
                     <input type="file" id="fileInputTxt" accept=".txt" style="display: none;" />
