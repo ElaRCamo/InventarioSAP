89e27a28d61b0d2c100381e21bf605a362121251
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-22 08:15:58 -0600

    insert txt Storage_Unit

--- dao/connection.php
+++ dao/connection.php
@@ -5,14 +5,13 @@ class LocalConector{
     private $usuario = "u909553968_UserInventario";
     private $clave = "Grammer2024Inventario";
     private $db = "u909553968_Inventario";
-    private $conexion;
 
     public function conectar(){
-        $this->conexion = mysqli_connect($this->host, $this->usuario, $this->clave, $this->db);
-        if ($this->conexion->connect_error) {
-            die("Error de conexión: " . $this->conexion->connect_error);
+        $conexion = mysqli_connect($this->host, $this->usuario, $this->clave, $this->db);
+        if ($conexion->connect_error) {
+            die("Error de conexión: " . $conexion->connect_error);
         }
         //echo "conexion exitosa";
-        return $this->conexion;
+        return $conexion;
     }
 }
