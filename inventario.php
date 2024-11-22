296f2e8bbcec533531cad02914be802aff89ffdc
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-22 08:07:14 -0600

    insert txt Storage_Unit

--- inventario.php
+++ inventario.php
@@ -77,9 +77,9 @@
                     <h2 class="text-center">Tabla Storage</h2>
                     <button class="btn btn-secondary text-right btnExcel" id="btnExcelStorage"> Cargar Excel Storage</button>
                     <input type="file" id="fileInputStorage" accept=".xlsx, .xls" style="display: none;" />
-                    <button class="btn btn-secondary text-right btnExcel" id="tooltipStorage"><i class="far fa-question-circle position-absolute"></i>? Ejemplo excel</button>
+                    <button class="btn btn-success text-right btnExcel" id="tooltipStorage"><i class="far fa-question-circle position-absolute"></i>? Ejemplo excel</button>
 
-                    <button class="btn btn-secondary text-right btnExcel" id="btnTxtStorage"> Actualizar txt </button>
+                    <button class="btn btn-primary text-right btnExcel" id="btnTxtStorage"> Actualizar txt </button>
                     <input type="file" id="fileInputTxtS" accept=".txt" style="display: none;" />
 
                     <table class="table table-striped table-bordered mt-3" id="tablaStorage">
